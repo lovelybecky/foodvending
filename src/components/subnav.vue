4ae9b178a3158d5b289d8ef23524a53818d20f4a
lovelybecky <740325450@qq.com>
2019-12-16 11:46:13 +0800

    second

---
+++ src/components/subnav.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="subnav">
+		<div class="selector">
+			<img class="subicon" src="../assets/home.png" >
+			<div>主页</div>
+		</div>
+		<div class="selector">
+			<img class="subicon" src="../assets/scan.png">
+			<div>扫描</div>
+		</div>
+		<div class="selector">
+			<img class="subicon" src="../assets/my.png">
+			<div>我的</div>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	#subnav{
+		width: 100%;
+		display: flex;
+		justify-content: space-around;
+		background-color: #F5F5F5;
+		margin: 0;
+		position: fixed;
+		bottom: 0;
+	}
+	.selector{
+		display: flex;
+		flex-direction: column;
+		margin: 10px;
+	}
+	.subicon{
+		height: 30px;
+		width: 30px;
+	}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -8,43 +8,27 @@
 		</div>
 		<div>new1</div>
 		<div>new2</div>
-		<div id="subnav">
-			<div class="selector">
-				<img class="subicon" src="../../assets/home.png" >
-				<div>主页</div>
-			</div>
-			<div class="selector">
-				<img class="subicon" src="../../assets/scan.png">
-				<div>扫描</div>
-			</div>
-			<div class="selector">
-				<img class="subicon" src="../../assets/my.png">
-				<div>我的</div>
-			</div>
-		</div>
+		<subNav></subNav>
 	</div>
 </template>
 
 <script>
+	import subNav from '../../components/subnav.vue';
+	export default{
+		components:{
+			subNav
+		},
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			
+		}
+	}
 </script>
 
 <style>
-	#subnav{
-		width: 100%;
-		display: flex;
-		justify-content: space-around;
-		background-color: #F5F5F5;
-		margin: 0;
-		position: fixed;
-		bottom: 0;
-	}
-	.selector{
-		display: flex;
-		flex-direction: column;
-		margin: 10px;
-	}
-	.subicon{
-		height: 30px;
-		width: 30px;
-	}
+	
 </style>
